fb23331b8dfe406c1ededeef96df4c2e83ca1e88
Mazda94 <m.alli9401@gmail.com>
2020-02-06 22:10:06 +0700

    Update sever

--- server/app.js
+++ server/app.js
@@ -1,7 +1,7 @@
+require('dotenv').config()
 const express = require('express')
 const app = express()
 const port = 3000
-const dotenv = require('dotenv').config()
 const cors = require('cors')
 
 app.use(cors())
--- server/routes/index.js
+++ server/routes/index.js
@@ -1,8 +1,8 @@
 const router = require('express').Router()
-
+const userController = require('../controllers/user')
 // tambahin sendiri routernya
 const memeRouter = require('./meme')
 
 router.use('/memes', memeRouter)
-
+router.post('/g-sign', userController.googleSignIn)
 module.exports = router
