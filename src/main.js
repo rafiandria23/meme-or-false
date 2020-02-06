68f8e3de7a0d0ca6be59588b31067545c04f4059
ajengrf <ajengrf.12@gmail.com>
2020-02-07 01:21:51 +0700

    modify static translate yandex

--- src/main.js
+++ src/main.js
@@ -1,69 +1,65 @@
-// Ini rencananya udah sama translate yandexnya
-// function getQuestions() {
-//   let question;
-//   $.ajax({
-//     type: "GET",
-//     url: "http://localhost:3000/trivia/",
-//     success: function (result) {
-//       let i = Math.floor(Math.random() * result.length);
-//       question = result[i].question;
-//       $.ajax({
-//         type: "POST",
-//         url: "http://localhost:3000/yandex/",
-//         data: {
-//           question: question
-//         },
-//         success: function (response) {
-//           console.log(response);
-//           $("#question").html(response.result);
-//           // append ke target tag nya
-//         }
-//       });
-//     },
-//     error: function (err) {
-//       console.log(err);
-//     }
-//   });
-// }
+class Play {
+  static getQuestions() {
+    $('#trivia').empty()
+    $.ajax({
+      type: "GET",
+      url: "http://localhost:3000/trivia/",
+      success: function (result) {
+        let i = Math.floor(Math.random() * result.length);
+        let trivia = result[i]
 
-let showQuestion = $('#trivia')
-function getQuestion() {
-  showQuestion.empty()
-  $.ajax({
-    method: 'GET',
-    url: `http://localhost:3000/trivia`,
-    success: function(response) {
-      let i = Math.floor(Math.random() * 10)
-    }
-  })
-}
+        Play.translateQuestion(trivia)
 
-getQuestion()
-
-function templateQuestion(question) {
-  let template = `
-      <div class="card" style="width: 50rem;>
-        <div class="card-body">
-        <h5 class="card-title">Question #1</h5>
-        <div class="putQuestion">
-          <p class="card-text" id="question">${question.question}</p>
-        </div>
-        <input class="btn btn-primary" type="button" value="True" onclick="checkAnswer(true, '${question.correct_answer}')">
-          <input class="btn btn-primary" type="button" value="False" onclick="checkAnswer(false, '${question.correct_answer}')">
-        </div>
-      </div>`
-
-  return template
-}
-
-function checkAnswer(userAnswer, correctAnswer) {
-  correctAnswer = correctAnswer == 'True' ? true : false
-  if (userAnswer == correctAnswer) {
-    console.log('benar')
-    console.log(userAnswer, correctAnswer)
-  } else {
-    console.log('salah')
-    console.log(userAnswer, correctAnswer)
-    getQuestion()
+      },
+      error: function (err) {
+        console.log(err);
+      }
+    });
   }
+
+  static translateQuestion(trivia) {
+    $.ajax({
+      type: "POST",
+      url: "http://localhost:3000/yandex/",
+      data: {
+        question: trivia.question
+      },
+      success: function (question) {
+        $('#trivia').append(Play.templateQuestion(question.result, trivia.correct_answer))
+      }
+    });
+  }
+
+  static templateQuestion(question, answer) {
+    let template = `
+          < div class="container" >
+            <div class="hero__content">
+              <div class="card" style="width: 50em; padding-bottom: 2em;">
+                <div class="container">
+                <h2>Question #1</h2>
+                <p class="card-subtitle mb-2 text-muted" style="font-size: 24px;">
+                  ${question}
+                </p>
+                <input class="button button__accent" type="button" value="True" onclick="Play.checkAnswer(true, '${answer}')">
+                <input class="button button__delete" type="button" value="False" onclick="Play.checkAnswer(false, '${answer}')"></input>
+                </div>
+              </div>
+            </div>
+          </div >`
+
+    return template
+  }
+
+  static checkAnswer(userAnswer, correctAnswer) {
+    correctAnswer = correctAnswer == 'True' ? true : false
+    if (userAnswer == correctAnswer) {
+      console.log('benar')
+      console.log(userAnswer, correctAnswer)
+    } else {
+      console.log('salah')
+      console.log(userAnswer, correctAnswer)
+      Play.getQuestions()
+    }
+  }
+
 }
