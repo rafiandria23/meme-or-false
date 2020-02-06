fe80f530353a2a76d6639b250799f6d6fb0465a3
muhafaa <muhafa99@gmail.com>
2020-02-06 23:46:48 +0700

    fix package-log error

--- server/routes/index.js
+++ server/routes/index.js
@@ -3,14 +3,13 @@ const userController = require('../controllers/user')
 // tambahin sendiri routernya
 const memeRouter = require('./meme')
 const triviaRouter = require('./trivia')
-const yandexRouter = require("./yandex");
-const triviaRouter = require('./trivia')
+const yandexRouter = require('./yandex')
 
 router.post('/g-sign', userController.googleSignIn)
 
 router.use('/memes', memeRouter)
 router.use('/trivia', triviaRouter)
-router.use("/yandex", yandexRouter);
+router.use('/yandex', yandexRouter)
 router.use('/trivia', triviaRouter)
 
 module.exports = router
