f90c637bb35c2d0dea754a2ecc175989b8f7ce47
ajengrf <ajengrf.12@gmail.com>
2020-02-06 19:52:09 +0700

    send trivia question and answers

--- server/controllers/triviaController.js
+++ server/controllers/triviaController.js
@@ -1,13 +1,21 @@
 const axios = require('axios');
 
 class Trivia {
-  static getTrivia() {
+  static getTrivia(req, res, next) {
     axios({
       method: 'get',
       url: 'https://opentdb.com/api.php?amount=10&difficulty=easy&type=boolean'
     })
-      .then(response => {
-        console.log(response.data.results, '< ini response')
+      .then(({ data }) => {
+        let trivias = []
+        data.results.forEach(trivia => {
+          trivias.push({
+            question: trivia.question,
+            correct_answer: trivia.correct_answer,
+            incorrect_answer: trivia.incorrect_answers[0]
+          })
+        })
+        res.status(200).send(trivias)
       })
       .catch(err => {
         console.log(err, '< ini error')
