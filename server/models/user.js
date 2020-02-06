769568c619e28951c6d81486ec3a0588b747bfb9
Adam Rafiandri <rafiandria23@gmail.com>
2020-02-06 21:59:58 +0700

    - Changed User model scheme, inserted hooks beforeCreate for password encryption.
    - Generated Match model, which contains UserId and score.
    
    - Created authenticate middleware for verifying token and sending the payload.
    
    - Generated Match model for migration.

--- server/models/user.js
+++ server/models/user.js
@@ -1,11 +1,36 @@
 'use strict';
+
+const bcrypt = require("bcrypt");
+
 module.exports = (sequelize, DataTypes) => {
-  const User = sequelize.define('User', {
-    email: DataTypes.STRING,
-    password: DataTypes.STRING
-  }, {});
-  User.associate = function(models) {
+  const {
+    Model
+  } = sequelize.Sequelize;
+
+  class User extends Model {}
+
+  User.init({
+    email: {
+      type: DataTypes.STRING
+    },
+    password: {
+      type: DataTypes.STRING
+    }
+  }, {
+    hooks: {
+      beforeCreate: (user, options) => {
+        if (user.password) {
+          user.password = bcrypt.hashSync(user.password, 10);
+        }
+      }
+    },
+    sequelize
+  });
+
+  User.associate = function (models) {
     // associations can be defined here
+    User.hasOne(models.Match);
   };
+  
   return User;
 };
