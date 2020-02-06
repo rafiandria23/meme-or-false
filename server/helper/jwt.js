41da48db1d8b65aa6cc2bd4ef62d9383b60232ee
Adam Rafiandri <rafiandria23@gmail.com>
2020-02-07 00:05:10 +0700

    Reduced line space from 4 to 2

--- server/helper/jwt.js
+++ server/helper/jwt.js
@@ -1,12 +1,15 @@
 const jwt = require("jsonwebtoken")
 
 function generateToken(payload) {
-    console.log(payload)
-    return jwt.sign(payload, process.env.secret_key)
+  console.log(payload)
+  return jwt.sign(payload, process.env.secret_key)
 }
 
 function verifyToken(token) {
-    return jwt.verify(token, process.env.secret_key)
+  return jwt.verify(token, process.env.secret_key)
 }
 
-module.exports = { generateToken, verifyToken }
+module.exports = {
+  generateToken,
+  verifyToken
+}
