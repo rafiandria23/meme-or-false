0c1a451babc808ef4ce153a587272200a81c23ca
muhafaa <muhafa99@gmail.com>
2020-02-07 09:02:39 +0700

    merging fitur sambungin login -> homepage -> soal + translate -> meme

--- src/auth.js
+++ src/auth.js
@@ -1,67 +1,49 @@
-function onSignIn(googleUser) {
-    var id_token = googleUser.getAuthResponse().id_token;
-    $.ajax({
-        type: "post",
-        url: 'http://localhost:3000/g-sign',
-        data: { id_token },
-        success: function ({ token, email }) {
-            localStorage.setItem('accessToken', token)
-            localStorage.setItem('email', email)
-            userLoggedIn()
-        },
-        error: function (err) {
-            console.log(err)
-        }
-    });
-}
-
-function signOut() {
-    var auth2 = gapi.auth2.getAuthInstance();
-    auth2.signOut().then(function () {
-        localStorage.removeItem('accessToken')
-        localStorage.removeItem('email')
-        userLoggedOut()
-    });
-}
-
 var $landingPage = $('#landing_page')
 var $mainPage = $('#main_page')
 var $start = $('#start')
-var $quizPage = $('#quiz_page')
 var $email = $('#email')
 
-$start.on('click', function (e) {
-    e.preventDefault()
-    $mainPage.fadeOut(500, function () {
-        $mainPage.hide()
-        $quizPage.fadeIn(500, function () {
-            $quizPage.show()
-        })
-    })
-})
+function onSignIn(googleUser) {
+  var id_token = googleUser.getAuthResponse().id_token
+  $.ajax({
+    type: 'post',
+    url: 'http://localhost:3000/g-sign',
+    data: { id_token },
+    success: function({ token, email }) {
+      localStorage.setItem('accessToken', token)
+      localStorage.setItem('email', email)
+      userLoggedIn()
+    },
+    error: function(err) {
+      console.log(err)
+    }
+  })
+}
+
+function signOut() {
+  var auth2 = gapi.auth2.getAuthInstance()
+  auth2.signOut().then(function() {
+    localStorage.removeItem('accessToken')
+    localStorage.removeItem('email')
+    userLoggedOut()
+  })
+}
 
 function userLoggedIn() {
-    $landingPage.fadeOut(500, function () {
-        $landingPage.hide()
-        $quizPage.hide()
-        $mainPage.fadeIn(500, function () {
-            $mainPage.show()
-            $email.text(`Welcome, ${localStorage.getItem('email')}`)
-        })
+  $landingPage.fadeOut(500, function() {
+    $mainPage.show(500, function() {
+      $email.text(`Welcome, ${localStorage.getItem('email')}`)
     })
+  })
 }
 
 function userLoggedOut() {
-    $mainPage.fadeOut(500, function () {
-        $mainPage.hide()
-        $quizPage.hide()
-        $landingPage.fadeIn(500, function () {
-            $landingPage.show()
-        })
-    })
+  $mainPage.hide()
+  $landingPage.show('fast')
 }
+
 if (localStorage.getItem('accessToken')) {
-    userLoggedIn()
+  userLoggedIn()
 } else {
-    userLoggedOut()
+  userLoggedOut()
 }
--- src/main.js
+++ src/main.js
@@ -7,18 +7,7 @@
 //     success: function (result) {
 //       let i = Math.floor(Math.random() * result.length);
 //       question = result[i].question;
-//       $.ajax({
-//         type: "POST",
-//         url: "http://localhost:3000/yandex/",
-//         data: {
-//           question: question
-//         },
-//         success: function (response) {
-//           console.log(response);
-//           $("#question").html(response.result);
-//           // append ke target tag nya
-//         }
-//       });
+//
 //     },
 //     error: function (err) {
 //       console.log(err);
@@ -26,32 +15,78 @@
 //   });
 // }
 
-let showQuestion = $('#trivia')
+var $start = $('#start')
+var $quizPage = $('#quiz_page')
+var $meme = $('#meme')
+
+// Button start buat mulai game
+$start.on('click', function(e) {
+  e.preventDefault()
+  $mainPage.fadeOut(500, function() {
+    $mainPage.hide()
+    getQuestion()
+    $quizPage.fadeIn(500, function() {
+      showQuestion()
+    })
+  })
+})
+
 function getQuestion() {
-  showQuestion.empty()
+  $quizPage.empty()
   $.ajax({
     method: 'GET',
     url: `http://localhost:3000/trivia`,
-    success: function(response) {
+    success: function(questions) {
       let i = Math.floor(Math.random() * 10)
+      let randomQuestion = questions[i]
+      $.ajax({
+        type: 'POST',
+        url: 'http://localhost:3000/yandex/',
+        data: {
+          question: randomQuestion.question
+        },
+        success: function(response) {
+          randomQuestion.question = response.result
+          console.log(randomQuestion)
+          $quizPage.append(templateQuestion(randomQuestion))
+        },
+        error: function(err) {
+          console.log(err)
+        }
+      })
+      showQuestion()
+    },
+    error: function(err) {
+      console.log(err)
     }
   })
 }
 
-getQuestion()
+function showQuestion() {
+  $quizPage.fadeIn(500)
+}
 
 function templateQuestion(question) {
   let template = `
-      <div class="card" style="width: 50rem;>
-        <div class="card-body">
-        <h5 class="card-title">Question #1</h5>
-        <div class="putQuestion">
-          <p class="card-text" id="question">${question.question}</p>
+      <div class="hero__overlay" style="background-color: blue;"></div>
+      <div class="hero__mask"></div>
+      <div class="hero__inner">
+        <div class="container">
+          <div class="hero__content">
+            <div class="card" style="width: 50em; padding-bottom: 2em; background-color: rgba(3, 0, 0, 0.418);">
+              <div class="container text-center">
+                <h2 class="text-center text-white">Question</h2>
+                <p class="card-subtitle mb-2 text-white" style="font-size: 24px;">
+                  ${question.question}
+                </p>
+                <button class="button button__accent" id="true" onclick="checkAnswer(true, '${question.correct_answer}')">Benar</button>
+                <button class="button button__delete" id="false" onclick="checkAnswer(false, '${question.correct_answer}')">Salah</button>
+              </div>
+            </div>
+          </div>
         </div>
-        <input class="btn btn-primary" type="button" value="True" onclick="checkAnswer(true, '${question.correct_answer}')">
-          <input class="btn btn-primary" type="button" value="False" onclick="checkAnswer(false, '${question.correct_answer}')">
-        </div>
-      </div>`
+      </div>
+      `
 
   return template
 }
@@ -59,11 +94,17 @@ function templateQuestion(question) {
 function checkAnswer(userAnswer, correctAnswer) {
   correctAnswer = correctAnswer == 'True' ? true : false
   if (userAnswer == correctAnswer) {
-    console.log('benar')
-    console.log(userAnswer, correctAnswer)
+    // kalo bener tampilin meme nya
+    Meme.getMeme()
+    $quizPage.fadeOut(500, function() {
+      $quizPage.hide()
+      $meme.fadeIn(500, function() {
+        $meme.fadeIn(500)
+      })
+    })
   } else {
-    console.log('salah')
-    console.log(userAnswer, correctAnswer)
+    // console.log('salah')
+    // console.log(userAnswer, correctAnswer)
     getQuestion()
   }
 }
