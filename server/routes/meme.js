35c441eb2e038997a35176d3c04d89e866d3dbc3
muhafaa <muhafa99@gmail.com>
2020-02-07 09:01:10 +0700

    merging fitur bikin kesinambungan dari login -> homepage -> soal+translate -> meme

--- server/routes/meme.js
+++ server/routes/meme.js
@@ -1,5 +1,18 @@
 const router = require('express').Router()
+const axios = require('axios').default
 
-router.get('/', function(req, res, next) {})
+router.get('/', function(req, res, next) {
+  axios({
+    method: 'GET',
+    url: `http://api.giphy.com/v1/gifs/search?api_key=${process.env.MEME_API_KEY}&q=meme`
+  })
+    .then(({ data }) => {
+      const index = Math.floor(Math.random() * data.data.length)
+      res.status(200).json(data.data[index].images.original.mp4)
+    })
+    .catch((err) => {
+      next(err)
+    })
+})
 
 module.exports = router
