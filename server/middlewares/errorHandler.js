861890235ece353e0d9ceb84bed4a0217d2e2c89
Mazda94 <m.alli9401@gmail.com>
2020-02-07 10:29:32 +0700

    Add higest score fitur

--- server/middlewares/errorHandler.js
+++ server/middlewares/errorHandler.js
@@ -1,6 +1,6 @@
 module.exports = (err, req, res, next) => {
   if (err) {
-    res.status(err.status).json({ message: err.message });
+    res.status(500).json({ message: err.message });
   }
   else {
     next();
